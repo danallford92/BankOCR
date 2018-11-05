ebc38771a26685c0caa6a03a2fc449b057448680
Dan Allford <danallford_23@hotmail.com>
2018-11-05 23:15:39 +0000

    Add accpetance test with realistic number of account number to verify performance

--- src/fileParser.test.js
+++ src/fileParser.test.js
@@ -1,10 +1,16 @@
 const parseFile = require('./fileParser')
 const fs = require('fs')
 
-it('can parse a single account number', () => {
+it('can parse a single account number', () => 
     expect(parseFile('./test_fixtures/account-numbers/012345678.txt')).toEqual(["012345678"])
-})
+)
 
-it('can parse two account number', () => {
+it('can parse two account number', () => 
     expect(parseFile('./test_fixtures/account-numbers/two_accounts.txt')).toEqual(["012345678", "123456789"])
+)
+
+it('can parse lots of accounts', () => {
+    const accountNumbers = parseFile('./test_fixtures/account-numbers/1250_accounts.txt')
+    expect(accountNumbers).toHaveLength(1250)
+    expect(accountNumbers[1249]).toEqual("012345678")
 })
